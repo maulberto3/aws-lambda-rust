84a6354a0012a43a73bb2ec5df2c5a85ee7aa0af
Mauricio <maulberto3@hotmail.com>
2024-03-19 05:45:15 +0000

    working cargo-lambda local

--- new-lambda-project/src/main.rs
+++ new-lambda-project/src/main.rs
@@ -1,53 +1,40 @@
-use lambda_runtime::{run, service_fn, Error, LambdaEvent};
+use lambda_http::{run, service_fn, tracing, Body, Error, Request, RequestExt, Response};
 
-use serde::{Deserialize, Serialize};
+// for a newly created cargo lambda project 
+// one terminal
+// cargo lambda watch
+// then on second terminal
+// curl http://localhost:9000/?name=John
+// should output
+// "Hello John, this is an AWS Lambda HTTP request"
 
-/// This is a made-up example. Requests come into the runtime as unicode
-/// strings in json format, which can map to any structure that implements `serde::Deserialize`
-/// The runtime pays no attention to the contents of the request payload.
-#[derive(Deserialize)]
-struct Request {
-    command: String,
-}
-
-/// This is a made-up example of what a response structure may look like.
-/// There is no restriction on what it can be. The runtime requires responses
-/// to be serialized into json. The runtime pays no attention
-/// to the contents of the response payload.
-#[derive(Serialize)]
-struct Response {
-    req_id: String,
-    msg: String,
-}
 
 /// This is the main body for the function.
 /// Write your code inside it.
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-/// - https://github.com/aws-samples/serverless-rust-demo/
-async fn function_handler(event: LambdaEvent<Request>) -> Result<Response, Error> {
-    // Extract some useful info from the request
-    let command = event.payload.command;
+async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+    // dbg!(&event);
+    // Extract some useful information from the request
+    let who = event
+        .query_string_parameters_ref()
+        .and_then(|params| params.first("name"))
+        .unwrap_or("world");
+    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
 
-    // Prepare the response
-    let resp = Response {
-        req_id: event.context.request_id,
-        msg: format!("Command {}.", command),
-    };
-
-    // Return `Response` (it will be serialized to JSON automatically by the runtime)
+    // Return something that implements IntoResponse.
+    // It will be serialized to the right response event automatically by the runtime
+    let resp = Response::builder()
+        .status(200)
+        .header("content-type", "text/html")
+        .body(message.into())
+        .map_err(Box::new)?;
     Ok(resp)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
-        // disable printing the name of the module in every log line.
-        .with_target(false)
-        // disabling time is handy because CloudWatch will add the ingestion time.
-        .without_time()
-        .init();
+    tracing::init_default_subscriber();
 
     run(service_fn(function_handler)).await
 }
